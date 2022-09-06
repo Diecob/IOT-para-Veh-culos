623e8bbc7a1bf72a732fd2021864cb07c23a4e08
Diecob <112907709+Diecob@users.noreply.github.com>
2022-09-06 12:56:31 -0300

    Update Edgent_ESP8266_v3.ino

--- Edgent_ESP8266_v3.ino
+++ Edgent_ESP8266_v3.ino
@@ -1,5 +1,5 @@
-#define BLYNK_TEMPLATE_ID "TMPLQoiNncJM"
-#define BLYNK_DEVICE_NAME "auto"
+#define BLYNK_TEMPLATE_ID "*************"
+#define BLYNK_DEVICE_NAME "*************"
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 #define BLYNK_PRINT Serial
 #define APP_DEBUG
